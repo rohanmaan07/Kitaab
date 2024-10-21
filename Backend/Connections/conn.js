40e52bc5065ac40cd1aeb8eb4fa7bdc6ec9f43e8
Rohan Mandal <139258819+rohanmaan07@users.noreply.github.com>
2024-10-21 18:46:08 +0530

    Update conn.js

--- Backend/Connections/conn.js
+++ Backend/Connections/conn.js
@@ -1,16 +1,17 @@
-const mongoose = require('mongoose');
-
-const connectDB = async () => {
+const mongoose = require("mongoose");
+const uri = process.env.ATLAS_DB;
+async function main() {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      connectTimeoutMS: 30000,
+      socketTimeoutMS: 45000,
     });
-    console.log("MongoDB connected successfully.");
+    console.log("Connected to MongoDB!");
   } catch (error) {
-    console.error("Error connecting to MongoDB:", error.message);
-    throw error; // Rethrow the error to be caught in app.js
+    console.error("Error connecting to MongoDB:", error);
   }
-};
+}
 
-module.exports = connectDB; // Export the connectDB function
+module.exports = main;
