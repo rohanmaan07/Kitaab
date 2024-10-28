5b7153b802e15e9f89466a0127190cff75af01a0
Rohan Mandal <139258819+rohanmaan07@users.noreply.github.com>
2024-10-28 21:12:14 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -7,12 +7,12 @@ const Book = require("./Routes/book");
 const Fav = require("./Routes/fav");
 const Order = require("./Routes/order");
 const Cart = require("./Routes/cart");
+const payment=require("./Routes/payment");
 const main = require("./Connections/conn");
 require("dotenv").config();
 
-const _dirname = path.resolve();
+const _dirname=path.resolve();
 
-// Connect to MongoDB
 main()
   .then(() => {
     console.log("MongoDB connected successfully..");
@@ -21,31 +21,23 @@ main()
     console.error("Error connecting to MongoDB:", err);
   });
 
-// Middleware
+
 app.use(express.json());
 
-// CORS configuration
-const allowedOrigins = ['https://kitaabrohan.onrender.com']; 
-app.use(cors({
-  origin: allowedOrigins,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true, 
-}));
-
 // Routes
+app.use(cors());
 app.use("/api/v1", User);
 app.use("/api/v1", Book);
 app.use("/api/v1", Fav);
 app.use("/api/v1", Order);
 app.use("/api/v1", Cart);
+app.use("/api/v1", payment);
 
-// Serve static files from the Frontend dist directory
-app.use(express.static(path.join(_dirname, "/Frontend/dist")));
-app.get('*', (_, res) => {
-  res.sendFile(path.resolve(_dirname, "Frontend", "dist", "index.html"));
+app.use(express.static(path.join(_dirname,"/Frontend/dist")));
+app.get('*',(_,res)=>{
+  res.sendFile(path.resolve(_dirname,"Frontend","dist","index.html"));
 });
 
-// Start server
 app.listen(8080, () => {
   console.log("Server is listening on port 8080..");
 });
