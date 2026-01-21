68cf72a03cc9f2938f8fe0cb6b4f0c38f37b558a
Rohan Maan <rohanmandal2208@gmail.com>
2026-01-21 17:33:58 +0530

    improve Home page layout and button alignment

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -7,17 +7,17 @@ function Home() {
       <>
         <div className="bg-black text-white flex flex-col md:flex-row items-center justify-center">
           <div className="flex flex-col justify-center items-start text-left p-7 md:w-1/2">
-            <h1 className=" lg:text-5xl font-bold mb-6 md:text-2xl font-bold mb-6 ">
+            <h1 className=" lg:text-4xl font-bold mb-6 md:text-2xl mx-auto font-bold mb-6 ">
               Welcome to Kitaab..
             </h1>
-            <p className=" lg:text-lg text-gray-300 mb-1 ">
+            <p className=" lg:text-lg text-gray-300 mx-auto mb-1 ">
             Kisi ki yaad ne humko yun bhega diya, <br></br>
             Jaise kisi ne paani mein rang milaa diya
             </p>
-            <p className="text-lg text-gray-400 ml-20 mb-5">~ Ahmad Faraz</p>
+            <p className="text-sm text-gray-400  ml-[10rem] mb-5">~ Ahmad Faraz</p>
             <a
               href="/all-books"
-              className=" bg-[#E50914] px-6 py-3 rounded text-white hover:bg-opacity-90 transition"
+              className="block mx-auto bg-[#E50914] px-6 py-3 rounded text-white hover:bg-opacity-90 transition"
             >
               Browse Books
             </a>
